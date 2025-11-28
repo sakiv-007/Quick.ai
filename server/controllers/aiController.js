92c33e04dd1a39bdacc2c37eac6af95a6f7c5c32
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-11-28 19:35:17 +0530

    upto image gen its working now

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -2,6 +2,9 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
+import FormData from "form-data";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -47,5 +50,100 @@ export const generateArticle = async (req, res)=>{
     } catch (error) {
         console.log(error.message)
         res.json({ success: false, message: error.message})
+    }
+}
+
+
+export const generateBlogTitle = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if(plan !== 'premium' && free_usage >=10){
+            return res.json({ success: false, message: "Limit reacehd. Upgrade to continue."})
+        }
+
+        const response = await AI.chat.completions.create({
+        model: "gemini-2.0-flash",
+        messages: [
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+        temperature: 0.7,
+        max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+    if(plan !== 'premium'){
+        await clerkClient.users.updateUserMetadata(userId, {
+            privateMetadata: {
+                free_usage: free_usage + 1
+            }
+        })
+    }
+
+    res.json({ success: true, content})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message})
+    }
+}
+
+
+export const generateImage = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+        const plan = req.plan;
+        
+
+        if(plan !== 'premium'){
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions."})
+        }
+
+        const formData  = new FormData();
+        formData.append('prompt', prompt);
+
+        const headers = {
+            ...formData.getHeaders(),
+            'x-api-key': process.env.CLIPDROP_API_KEY,
+            'accept': 'image/png'
+        };
+
+        const { data, status } = await axios.post(
+            "https://clipdrop-api.co/text-to-image/v1",
+            formData,
+            {
+                headers,
+                responseType: "arraybuffer",
+                validateStatus: () => true,
+            }
+        );
+
+        if (status >= 400 || !data) {
+            return res.status(400).json({ success: false, message: "Clipdrop returned an error" });
+        }
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+        const { secure_url } = await cloudinary.uploader.upload(base64Image)
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type, publish ) VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false })`;
+
+    
+
+    res.json({ success: true, content: secure_url})
+
+    } catch (error) {
+        console.log(error?.response?.data || error.message);
+        const message = error?.response?.data?.message || error.message || "Unexpected error";
+        res.status(400).json({ success: false, message });
     }
 }
